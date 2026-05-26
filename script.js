0cc948fb646e4a605bb8725179ab86859ecdb94d
SutiruKarasu <kolgeciedi5@gmail.com>
2026-05-27 00:33:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -200,3 +200,17 @@ const albumSwiper = new Swiper('.album-swiper', {
         separateDialCode: true
     });
 
+const infoBtn = document.getElementById('info-btn');
+const infoWindow = document.getElementById('info-window');
+if (infoBtn && info windows) {
+    infoBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        infoWindow.classList.toggle('show');
+    });
+
+    document.AddEventListener('click'), () => {
+        infoWindow.classList.remove('show');
+    });
+}
+
+
